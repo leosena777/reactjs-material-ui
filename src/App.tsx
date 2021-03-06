66b67d74eaae688350f3b2142f6411b4ad94bc34
Leandro Sena <leo.sena777@gmail.com>
2021-03-06 16:50:58 -0300

    project config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState, useEffect } from "react";
-import "./App.css";
-import DonutLargeIcon from "@material-ui/icons/DonutLarge";
-import ChatIcon from "@material-ui/icons/Chat";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
-import SearchIcon from "@material-ui/icons/Search";
+import { useState, useEffect } from 'react';
+import './App.css';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
 
-import { ChatListItem, ChatIntro, ChatWindow } from "./components";
+import { ChatListItem, ChatIntro, ChatWindow } from './components';
 
 export interface ChatList {
   chatId: number;
@@ -17,18 +17,18 @@ function App(): JSX.Element {
   const [chatList, setChatList] = useState<ChatList[]>([
     {
       chatId: 1,
-      name: "Leandro sena",
-      img: "https://www.w3schools.com/howto/img_avatar.png",
+      name: 'Leandro sena',
+      img: 'https://www.w3schools.com/howto/img_avatar.png',
     },
     {
       chatId: 2,
-      name: "Maria",
-      img: "https://www.w3schools.com/w3images/avatar6.png",
+      name: 'Maria',
+      img: 'https://www.w3schools.com/w3images/avatar6.png',
     },
     {
       chatId: 3,
-      name: "Julia",
-      img: "https://www.w3schools.com/w3images/avatar6.png",
+      name: 'Julia',
+      img: 'https://www.w3schools.com/w3images/avatar6.png',
     },
   ]);
   const [activeChat, setActiveChat] = useState<ChatList | null>(null);
@@ -52,23 +52,20 @@ function App(): JSX.Element {
           />
           <div className="header-buttons">
             <div className="header-btn">
-              <DonutLargeIcon style={{ color: "#919191" }} />
+              <DonutLargeIcon style={{ color: '#919191' }} />
             </div>
             <div className="header-btn">
-              <ChatIcon style={{ color: "#919191" }} />
+              <ChatIcon style={{ color: '#919191' }} />
             </div>
             <div className="header-btn">
-              <MoreVertIcon style={{ color: "#919191" }} />
+              <MoreVertIcon style={{ color: '#919191' }} />
             </div>
           </div>
         </header>
         <div className="search">
           <div className="search-input">
-            <SearchIcon fontSize="small" style={{ color: "#919191" }} />
-            <input
-              type="search"
-              placeholder="Procurar ou começar uma nova conversa"
-            />
+            <SearchIcon fontSize="small" style={{ color: '#919191' }} />
+            <input type="search" placeholder="Procurar ou começar uma nova conversa" />
           </div>
         </div>
         <div className="chatlist">
